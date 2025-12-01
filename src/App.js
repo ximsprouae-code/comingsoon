8a33e1669704e10d5ca520a7cbdb6a9e6ea33bad
ximsprouae-code <ximsprouae@gmail.com>
2025-12-01 15:37:59 +0400

    Update App.js
    yes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { ReactComponent as IconFacebook } from './assets/icons/facebook.svg';
 import { ReactComponent as IconTwitter } from './assets/icons/twitter.svg';
-// If you don't need GitHub icon, you can delete the next line + SVG file
-// import { ReactComponent as IconGithub } from './assets/icons/github.svg';
+// import { ReactComponent as IconGithub } from './assets/icons/github.svg'; // removed
 import logo from './assets/xims-logo.png';
 import "./App.css";
 
@@ -10,6 +9,7 @@ class App extends React.Component {
   render = () => {
     return (
       <div className="card">
+        {/* HEADER */}
         <div className="header">
           <div className="logo">
             <a href=".">
@@ -22,52 +22,29 @@ class App extends React.Component {
           </div>
 
           <div className="social">
-            <a
-              href="https://facebook.com"
-              title="Facebook"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
+            <a href="https://facebook.com" title="Facebook" target="_blank" rel="noopener noreferrer">
               <IconFacebook className="icon" />
             </a>
-            <a
-              href="https://twitter.com"
-              title="Twitter"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
+            <a href="https://twitter.com" title="Twitter" target="_blank" rel="noopener noreferrer">
               <IconTwitter className="icon" />
             </a>
-            {/* Remove GitHub icon (uncomment if you need it)
-            <a
-              href="https://github.com"
-              title="GitHub"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <IconGithub className="icon" />
-            </a>
-            */}
+            {/* GitHub removed */}
           </div>
         </div>
 
+        {/* MAIN SECTION */}
         <div className="content">
           <div className="title-holder">
-            <h1>Connecting Entities Through Technology.</h1>
-            <p>
-              Website coming soon. Check back for updates, or reach out to us by email.
-            </p>
+            <h1>Get ready for the change.</h1>
+            <p>Website coming soon. Please check back soon. Shoot us an email if you're curious.</p>
           </div>
 
-          {/* Button that opens a new email with your email id */}
-          <a
-            href="mailto:info@ximstechsolutions.com?subject=Website%20Enquiry&body=Hi%20XIMS%20Solutions%20%26%20Technologies,"
-          >
+          <a href="mailto:info@ximstechsolutions.com?subject=Website%20Enquiry&body=Hi%20XIMS%20Solutions%20%26%20Technologies,">
             <div className="cta">Send us an email</div>
           </a>
         </div>
 
-        {/* Footer removed – add your own if needed */}
+        {/* FOOTER REMOVED */}
       </div>
     );
   }
