21e51a0bc867ce5752851d0932798b35f12e74e4
ximsprouae-code <ximsprouae@gmail.com>
2025-12-01 15:24:18 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { ReactComponent as IconFacebook } from './assets/icons/facebook.svg';
 import { ReactComponent as IconTwitter } from './assets/icons/twitter.svg';
-import { ReactComponent as IconGithub } from './assets/icons/github.svg';
+// If you don't need GitHub icon, you can delete the next line + SVG file
+// import { ReactComponent as IconGithub } from './assets/icons/github.svg';
+import logo from './assets/xims-logo.png';
 import "./App.css";
 
 class App extends React.Component {
@@ -10,40 +12,62 @@ class App extends React.Component {
       <div className="card">
         <div className="header">
           <div className="logo">
-            {/* Replace LOGO with your brand name */}
-            <a href=".">XIMSTECH SOLUTIONS</a>
+            <a href=".">
+              <img
+                src={logo}
+                alt="XIMS Solutions & Technologies"
+                className="logo-img"
+              />
+            </a>
           </div>
 
           <div className="social">
-            <a href="https://facebook.com" title="Facebook" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://facebook.com"
+              title="Facebook"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <IconFacebook className="icon" />
             </a>
-            <a href="https://twitter.com" title="Twitter" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://twitter.com"
+              title="Twitter"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <IconTwitter className="icon" />
             </a>
-
-            {/* Remove GitHub icon if not needed */}
-            {/* <a href="https://github.com" title="GitHub" target="_blank" rel="noopener noreferrer">
+            {/* Remove GitHub icon (uncomment if you need it)
+            <a
+              href="https://github.com"
+              title="GitHub"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <IconGithub className="icon" />
-            </a> */}
+            </a>
+            */}
           </div>
         </div>
 
         <div className="content">
           <div className="title-holder">
-            <h1>Get ready for the change.</h1>
-            <p>Website coming soon. Please check back to know more. Shoot us an email if you're curious.</p>
+            <h1>Connecting Entities Through Technology.</h1>
+            <p>
+              Website coming soon. Check back for updates, or reach out to us by email.
+            </p>
           </div>
 
-          {/* Email auto-filled when clicked */}
-          <a 
-            href="mailto:info@ximstechsolutions.com?subject=Website%20Enquiry&body=Hi%20Ximstech%20Solutions,"
+          {/* Button that opens a new email with your email id */}
+          <a
+            href="mailto:info@ximstechsolutions.com?subject=Website%20Enquiry&body=Hi%20XIMS%20Solutions%20%26%20Technologies,"
           >
             <div className="cta">Send us an email</div>
           </a>
         </div>
 
-        {/* Footer removed completely */}
+        {/* Footer removed – add your own if needed */}
       </div>
     );
   }
