c6b9e3201d9f633b5afee1f0266f2ec7a2d16a8f
ximsprouae-code <ximsprouae@gmail.com>
2025-12-01 14:56:44 +0400

    Update App.js
    update code

--- src/App.js
+++ src/App.js
@@ -10,8 +10,10 @@ class App extends React.Component {
       <div className="card">
         <div className="header">
           <div className="logo">
-            <a href=".">LOGO</a>
+            {/* Replace LOGO with your brand name */}
+            <a href=".">XIMSTECH SOLUTIONS</a>
           </div>
+
           <div className="social">
             <a href="https://facebook.com" title="Facebook" target="_blank" rel="noopener noreferrer">
               <IconFacebook className="icon" />
@@ -19,23 +21,29 @@ class App extends React.Component {
             <a href="https://twitter.com" title="Twitter" target="_blank" rel="noopener noreferrer">
               <IconTwitter className="icon" />
             </a>
-            <a href="https://github.com/arkn98/coming-soon" title="GitHub" target="_blank" rel="noopener noreferrer">
+
+            {/* Remove GitHub icon if not needed */}
+            {/* <a href="https://github.com" title="GitHub" target="_blank" rel="noopener noreferrer">
               <IconGithub className="icon" />
-            </a>
+            </a> */}
           </div>
         </div>
+
         <div className="content">
           <div className="title-holder">
             <h1>Get ready for the change.</h1>
             <p>Website coming soon. Please check back to know more. Shoot us an email if you're curious.</p>
           </div>
-          <a href="mailto:user@example.com">
+
+          {/* Email auto-filled when clicked */}
+          <a 
+            href="mailto:info@ximstechsolutions.com?subject=Website%20Enquiry&body=Hi%20Ximstech%20Solutions,"
+          >
             <div className="cta">Send us an email</div>
           </a>
         </div>
-        <div className="footer">
-          <span>made by <a className="underlined" href="https://github.com/arkn98" target="_blank" rel="noopener noreferrer">arkn98</a> using <a className="underlined" href="https://reactjs.org/" title="ReactJS" target="_blank" rel="noopener noreferrer">React</a> | <a className="underlined" href="https://github.com/arkn98/coming-soon" title="GitHub repo" target="_blank" rel="noopener noreferrer">GitHub</a></span>
-        </div>
+
+        {/* Footer removed completely */}
       </div>
     );
   }
